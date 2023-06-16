f62d77c6123709c7c20792069274bfad4f47bd94
SandeshGC <gcsandesh01@gmail.com>
2023-06-16 21:07:08 +0545

    add static content

--- App.js
+++ App.js
@@ -1,24 +1,61 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
-  console.log('Hello World!')
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello world</Text>
-    </View>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text style={styles.currentTemp}>45'C</Text>
+        <Text style={styles.feelsLikeTemp}>Feels like 50'C</Text>
+
+        <View style={styles.highLowWrapper}>
+          <Text>High: 46'C</Text>
+          <Text>Low: 30'C</Text>
+        </View>
+      </View>
+
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>It's very hot!</Text>
+        <Text style={styles.message}>It's perfect for swimming.</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: 'aliceblue'
+  },
   container: {
     flex: 1,
-    backgroundColor: 'aliceblue',
     alignItems: 'center',
     justifyContent: 'center'
   },
-  text: {
-    color: '#444444'
+  highLowWrapper: {
+    flexDirection: 'row'
+  },
+  currentTemp: {
+    fontSize: 50,
+    fontWeight: '900'
+  },
+  feelsLikeTemp: {
+    fontSize: 30,
+    color: 'black'
+  },
+  highLowTemp: {
+    color: 'black',
+    fontSize: 20
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    paddingLeft: 20,
+    marginBottom: 40
+  },
+  description: {
+    fontSize: 48
+  },
+  message: {
+    fontSize: 30
   }
 })
